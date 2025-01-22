65bb608896e2eb48f2780fe41395c739a1c6d903
gykim93 <gykim1993@gmail.com>
2025-01-22 19:24:14 +0900

    테일윈드 도입

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Navigate, NavLink, Route, Routes } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 import WritePage from "./pages/WritePage";
 
 function App() {
   return (
     <>
-      <ListPage />
-      <WritePage />
+      <header className="flex">
+        <NavLink className="font-bold p-3 hover:text-red-500" to="/list">
+          리스트
+        </NavLink>
+        <NavLink className="font-bold p-3 hover:text-red-500" to="/write">
+          작성
+        </NavLink>
+      </header>
+      <Routes>
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="*" element={<Navigate to="/list" />} />
+      </Routes>
     </>
   );
 }
